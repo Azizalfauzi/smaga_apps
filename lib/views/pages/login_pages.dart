a9c15717cd85fc544cd31227fb32abbd3c15c0e1
Aziz Alfauzi <50761836+Azizalfauzi@users.noreply.github.com>
2022-05-09 15:07:40 +0700

    adding pages login,onboard,splashscreen

---
+++ lib/views/pages/login_pages.dart
@@ -0,0 +1,15 @@
+part of 'pages.dart';
+
+class LoginPages extends StatefulWidget {
+  const LoginPages({Key? key}) : super(key: key);
+
+  @override
+  State<LoginPages> createState() => _LoginPagesState();
+}
+
+class _LoginPagesState extends State<LoginPages> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
